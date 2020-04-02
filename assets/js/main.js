8a43241c78883acef44a5f409acc5f0e3da400d6
Kenneth <kennethvdstrieckt@hotmail.com>
2020-04-03 00:13:57 +0200

    switching bg & gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,4 +48,22 @@
 					target: $body,
 					visibleClass: 'navPanel-visible'
 				});
+				
+				$(function () {
+					var body = $('#banner');
+					var backgrounds = [
+					  'url(https://images.pexels.com/photos/544965/pexels-photo-544965.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)', 
+					  'url(https://images.pexels.com/photos/1078884/pexels-photo-1078884.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)'];
+					var current = 0;
+				
+					function nextBackground() {
+						body.css(
+							'background',
+						backgrounds[current = ++current % backgrounds.length]);
+				
+						setTimeout(nextBackground, 10000);
+					}
+					setTimeout(nextBackground, 10000);
+					body.css('background', backgrounds[0]);
+				});
 })(jQuery);
