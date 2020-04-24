bdd4847b24c0e0ddad434b055cce737d970832a6
Kenneth <kennethvdstrieckt@hotmail.com>
2020-04-25 00:58:39 +0200

    gallery restructuring

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,21 +49,5 @@
 					visibleClass: 'navPanel-visible'
 				});
 				
-				$(function () {
-					var body = $('#banner');
-					var backgrounds = [
-					  'url("../../assets/images/projecten/machine7.jpg")', 
-					  'url("../../assets/images/projecten/project2.JPG")'];
-					var current = 0;
 				
-					function nextBackground() {
-						body.css(
-							'background-image',
-						backgrounds[current = ++current % backgrounds.length]);
-				
-						setTimeout(nextBackground, 10000);
-					}
-					setTimeout(nextBackground, 10000);
-					body.css('background-image', backgrounds);
-				});
 })(jQuery);
