eae76c3589140f4d1a92a8c766bcf6dc230b4724
Kenneth <kennethvdstrieckt@hotmail.com>
2020-09-29 12:08:34 +0200

    gallery function

--- views/gallery/assets/js/galleryselection.js
+++ views/gallery/assets/js/galleryselection.js
@@ -1,10 +1,28 @@
-function gallerySelector() {
-    var selectedGallery = document.getElementById("main");
-    var selectedList = document.getElementById("mediaLinkList");
-    
-    if (selectedGallery.class === selectedList.value) {
-        selectedGallery.style.display = "block";
+document.getElementById('mediaLinkList').addEventListener('click', function () {
+    toggle(document.querySelectorAll('#main'));
+});
+
+function toggle (elements, specifiedDisplay) {
+  var element, index;
+  var selectedGallery = document.getElementById("main");
+  var selectedButton = document.getElementById("mediaLinkList");
+
+  elements = elements.length ? elements : [elements];
+  for (index = 0; index < elements.length; index++) {
+    element = elements[index];
+
+    if (isElementHidden(element)) {
+      element.style.display = '';
+
+      // If the element is still hidden after removing the inline display
+      if (selectedButton.id === selectedGallery.class) {
+        element.style.display = specifiedDisplay || 'flex';
+      }
     } else {
-        selectedGallery.style.display = "none";
+      element.style.display = 'none';
     }
+  }
+  function isElementHidden (element) {
+    return window.getComputedStyle(element, null).getPropertyValue('display') === 'none';
+  }
 }
