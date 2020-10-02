d396266d8ff08ec68955d3b52130209956d35d7f
Kenneth <kennethvdstrieckt@hotmail.com>
2020-10-02 13:30:55 +0200

    gallery & sidebar fix

--- views/gallery/assets/js/galleryselection.js
+++ views/gallery/assets/js/galleryselection.js
@@ -1,28 +1,10 @@
-document.getElementById('mediaLinkList').addEventListener('click', function () {
-    toggle(document.querySelectorAll('#main'));
+
+$(function () {
+  $('#playground div').hide().eq(0).show();
+  
+  $('#mediaLinkList li').click(function () {
+      $('#playground div').hide()
+      num = $('#mediaLinkList li').index(this);
+      $('#playground div').hide().eq(num).show();
+  });
 });
-
-function toggle (elements, specifiedDisplay) {
-  var element, index;
-  var selectedGallery = document.getElementById("main");
-  var selectedButton = document.getElementById("mediaLinkList");
-
-  elements = elements.length ? elements : [elements];
-  for (index = 0; index < elements.length; index++) {
-    element = elements[index];
-
-    if (isElementHidden(element)) {
-      element.style.display = '';
-
-      // If the element is still hidden after removing the inline display
-      if (selectedButton.id === selectedGallery.class) {
-        element.style.display = specifiedDisplay || 'flex';
-      }
-    } else {
-      element.style.display = 'none';
-    }
-  }
-  function isElementHidden (element) {
-    return window.getComputedStyle(element, null).getPropertyValue('display') === 'none';
-  }
-}
